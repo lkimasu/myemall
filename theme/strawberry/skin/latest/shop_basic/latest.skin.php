d58af94bce0e5804f15537e41e0d944f605ab4d9
lkimasu <lwlstjdl@naver.com>
2024-10-17 15:05:35 +0900

    update h1,h2,h3 and style

--- theme/strawberry/skin/latest/shop_basic/latest.skin.php
+++ theme/strawberry/skin/latest/shop_basic/latest.skin.php
@@ -6,53 +6,51 @@ add_stylesheet('<link rel="stylesheet" href="'.$latest_skin_url.'/style.css">', 
 ?>
 
 <div class="lat">
-    <strong class="lat_title"><a href="<?php echo G5_BBS_URL ?>/board.php?bo_table=<?php echo $bo_table ?>"><?php echo $bo_subject ?></a></strong>
+    <div class="lat_title">
+        <h2>
+            <a href="<?php echo G5_BBS_URL ?>/board.php?bo_table=<?php echo $bo_table ?>">
+                <?php echo $bo_subject ?>
+            </a>
+        </h2>
+    </div>
+    
     <ul>
     <?php for ($i=0; $i<count($list); $i++) {  ?>
         <li>
-            <?php
-            //echo $list[$i]['icon_reply']." ";
-            echo "<a href=\"".$list[$i]['href']."\">";
-            if ($list[$i]['is_notice'])
-                echo "<strong>".$list[$i]['subject']."</strong>";
-            else
-                echo $list[$i]['subject'];
-
-            if ($list[$i]['comment_cnt'])
-                echo $list[$i]['comment_cnt'];
-
-            echo "</a>";
-
-            // if ($list[$i]['link']['count']) { echo "[{$list[$i]['link']['count']}]"; }
-            // if ($list[$i]['file']['count']) { echo "<{$list[$i]['file']['count']}>"; }
-
-            if (isset($list[$i]['icon_new']) && $list[$i]['icon_new']) echo " <span class=\"new_icon\">NEW</span>";
-            //if (isset($list[$i]['icon_hot'])) echo " <i class=\"fa fa-heart\" aria-hidden=\"true\"></i>" ;
-            //if (isset($list[$i]['icon_file'])) echo " <i class=\"fa fa-download\" aria-hidden=\"true\"></i>" ;
-            //if (isset($list[$i]['icon_link'])) echo " <i class=\"fa fa-link\" aria-hidden=\"true\"></i>" ;
-            //if (isset($list[$i]['icon_secret'])) echo " <i class=\"fa fa-lock\" aria-hidden=\"true\"></i>" ;
-             ?>
+            <div class="lat_title">
+                <h3> 
+                    <a href="<?php echo $list[$i]['href']; ?>" title="<?php echo htmlspecialchars($list[$i]['subject']); ?>">
+                        <?php 
+                        if ($list[$i]['is_notice']) 
+                            echo "<strong>".$list[$i]['subject']."</strong>";
+                        else 
+                            echo $list[$i]['subject']; 
+                        
+                        if ($list[$i]['comment_cnt']) 
+                            echo $list[$i]['comment_cnt']; 
+                        ?>
+                    </a>
+                </h3>
+                <?php if (isset($list[$i]['icon_new']) && $list[$i]['icon_new']) echo " <span class=\"new_icon\" aria-label=\"New post\">NEW</span>"; ?>
+            </div>
         </li>
-    <?php }  ?>
-    <?php if (count($list) == 0) { //게시물이 없을 때  ?>
+    <?php } ?>
+    <?php if (count($list) == 0) { // 게시물이 없을 때 ?>
     <li>게시물이 없습니다.</li>
-    <?php }  ?>
+    <?php } ?>
     </ul>
+    
     <a href="<?php echo G5_BBS_URL ?>/board.php?bo_table=<?php echo $bo_table ?>" class="lat_more">+ 더보기</a>
 </div>
-
 
 <script>
 $(document).ready(function(){
     $('.lat ul').show().bxSlider({
-        speed:800,
-        pager:false, 
-        controls:false,
-        auto:true,
-        mode:'fade'
-
+        speed: 800,
+        pager: false, 
+        controls: false,
+        auto: true,
+        mode: 'fade'
     });
 });
-
 </script>
-
