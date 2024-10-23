e6387bd6e3edb9d0de2a42ada29fb57545a7f355
lkimasu <lwlstjdl@naver.com>
2024-10-24 06:53:19 +0900

    main.10.skin.php 업데이트

--- theme/strawberry/skin/shop/basic/main.10.skin.php
+++ theme/strawberry/skin/shop/basic/main.10.skin.php
@@ -5,7 +5,7 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 add_stylesheet('<link rel="stylesheet" href="'.G5_SHOP_SKIN_URL.'/style.css">', 0);
 add_javascript('<script src="'.G5_THEME_JS_URL.'/jquery.shop.list.js"></script>', 10);
 
-add_stylesheet('<link rel="stylesheet" href="'.G5_JS_URL.'/swiper/swiper.min.css">', 0);
+add_stylesheet('<link rel="stylesheet" href="'.G5_JS_URL.'/swiper/swiper.min.css">', 1);
 add_javascript('<script src="'.G5_JS_URL.'/swiper/swiper.min.js"></script>', 10);
 ?>
 
