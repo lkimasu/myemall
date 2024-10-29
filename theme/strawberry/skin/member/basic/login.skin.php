2204b4b82c1a556ccf9d8f57ebeebaf868c4ab96
lkimasu <lwlstjdl@naver.com>
2024-10-29 14:38:13 +0900

    update title

--- theme/strawberry/skin/member/basic/login.skin.php
+++ theme/strawberry/skin/member/basic/login.skin.php
@@ -5,6 +5,8 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 0);
 ?>
 
+<title> 로그인 - 거창한무역 </title>
+
 <!-- 로그인 시작 { -->
 <div id="mb_login" class="mbskin">
     <h1><?php echo $g5['title'] ?></h1>
