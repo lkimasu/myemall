4265bc7aba999d29bc9427a60d7a19de894b959d
lkimasu <lwlstjdl@naver.com>
2024-11-06 16:41:04 +0900

    update card_slider hard coding to sql

--- theme/strawberry/mobile/skin/shop/basic/card_slider.skin.php
+++ theme/strawberry/mobile/skin/shop/basic/card_slider.skin.php
@@ -6,32 +6,26 @@ add_stylesheet('<link rel="stylesheet" href="'.G5_MSHOP_SKIN_URL.'/style.css">',
 add_javascript('<script src="'.G5_THEME_JS_URL.'/jquery.shop.list.js"></script>', 10);
 add_stylesheet('<link rel="stylesheet" href="'.G5_JS_URL.'/swiper/swiper.min.css">', 0);
 add_javascript('<script src="'.G5_JS_URL.'/swiper/swiper.min.js"></script>', 10);
-?>
 
+
+$cards = [];
+$sql = "SELECT story_id, story_alt, story_url FROM {$g5['g5_shop_story_table']} ORDER BY story_id DESC";
+$result = sql_query($sql);
+while ($row = sql_fetch_array($result)) {
+    $image_path = G5_DATA_URL . "/story/" . $row['story_id'];  // 이미지 경로 설정
+    $cards[] = [
+        "image" => $image_path,
+        "title" => $row['story_alt'],
+        "link"  => $row['story_url']
+    ];
+}
+
+?>
 
 <div class="swiper-container sw2">
     <div class="swiper-wrapper">
-        <?php
-        // 카드 데이터 배열
-        $cards = [
-            [
-                "image" => "/theme/strawberry/shop/img/apple.jpg",
-                "title" => "추석용 홍로 작업 중 입니다.",
-                "link"  => "https://www.instagram.com/p/C_zueY_zlnQ/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
-            ],
-            [
-                "image" => "/theme/strawberry/shop/img/sweet_potato.jpg",
-                "title" => "고구마 수확하고 있습니다.",
-                "link"  => "https://blog.naver.com/wpdlf943/223596927254"
-            ],
-            [
-                "image" => "/theme/strawberry/shop/img/onion.jpg",
-                "title" => "국내산 양파 판매 중입니다.",
-                "link"  => "https://www.instagram.com/p/DAUg73ozzry/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
-            ]
-        ];
-
-        // 카드 출력
+    <?php
+        // DB에서 가져온 카드 데이터 출력
         foreach ($cards as $card) {
             echo '<div class="swiper-slide card">';
             echo '<a href="' . $card["link"] . '" target="_blank">';
@@ -42,7 +36,6 @@ add_javascript('<script src="'.G5_JS_URL.'/swiper/swiper.min.js"></script>', 10)
         }
         ?>
     </div>
-
 </div>
 
 <script>
